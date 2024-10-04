3122e63de17db7613e55fec7ef18806ce299f588
unknown <akyasmahira221@gmail.com>
2024-10-04 17:57:27 +0700

    KonversiSuhu

---
+++ src/main/java/com/mycompany/konversisuhu/KonversiSuhu.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.konversisuhu;
+
+/**
+ *
+ * @author RPL 2
+ */
+public class KonversiSuhu {
+
+    public static void main(String[] args) {
+       int C=20;
+       
+       System.out.println("Suhu Dalam Celcius   =   "+C);
+       System.out.println("Suhu Dalam Reamur    =   "+(C*4/5));
+       System.out.println("Suhu Dalam Farenheit =   "+(C*9/5+32));
+       System.out.println("Suhu Dalam Kelfin    =   "+(C+273));
+    }
+}
